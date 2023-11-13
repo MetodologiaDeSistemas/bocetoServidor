4a8c0d8beb6b6831f2e4624054c7c2a0e156cb07
MetodologiaDeSistemas <met.sistemas@gmail.com>
2023-11-13 18:42:55 -0300

    Comentario en model y route

---
+++ src/Models/Model.js
@@ -0,0 +1,3 @@
+//Aqui se crea el modelo de datos que vamos a pasar a mongoDB 
+// es decir que se crean todos los atributos de cada registro de la BD
+// y luego lo exportamos para que se puedan usar en los otrod componentes
