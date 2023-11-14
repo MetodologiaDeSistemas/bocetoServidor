042add58fe5ee99c250f5d9b35d13d5161e50d1f
MetodologiaDeSistemas <met.sistemas@gmail.com>
2023-11-14 13:09:11 -0300

    creamos el post de hora

--- src/Models/Model.js
+++
@@ -1,3 +0,0 @@
-//Aqui se crea el modelo de datos que vamos a pasar a mongoDB 
-// es decir que se crean todos los atributos de cada registro de la BD
-// y luego lo exportamos para que se puedan usar en los otrod componentes
---
+++ src/Routes/Hora.js
@@ -0,0 +1,25 @@
+// Aqui se definiran las rutas 
+// esto quiere decir que en este componente vamos a creat las distintas peticiones
+// al servidor: (get/Post/Put/Delete) 
+// estos los utilizaremos en conjunto con los modelos creados en las otras carpetas 
+// en cada uno de los EndPoint (funciones de peticion)
+
+const express = require('express');
+const router = express.Router();
+const horarioSchema = require("../Models/Hora")
+
+
+
+router.post("/hora", (req,res) =>{
+    const hora = horarioSchema(req.body);
+    hora
+    .save()
+    .then((data)=>{
+        res.json(data)
+    })
+    .catch((error)=>{
+        res.json({message : error})
+    })
+})
+
+module.exports = router;
