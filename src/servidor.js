577503647895d5205aa15f2211d22882d129b521
chinex2510 <116128868+chinex2510@users.noreply.github.com>
2023-11-13 22:14:57 -0300

    Conexion con MONGODB ATLAS

---
+++ src/servidor.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+//routes
+app.get('/', (req, res) => {
+    res.send("Bienvenido a mi API");
+});
+
+//Conexion MongoDB
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log('Conectado a la base de datos'))
+    .catch((error) => console.error(error));
+
+
+app.listen(port, () => console.log('server listning on port ', port));
