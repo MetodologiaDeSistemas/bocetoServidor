5a4d7236500963587550f3bd422c4c4d5165b0d1
MetodologiaDeSistemas <met.sistemas@gmail.com>
2023-11-14 17:04:15 -0300

    Creamos el CRUD de Horario

--- src/Routes/Hora.js
+++ src/Routes/Hora.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const horarioSchema = require("../Models/Hora")
 
 
-
+//Crear
 router.post("/hora", (req,res) =>{
     const hora = horarioSchema(req.body);
     hora
@@ -21,10 +21,57 @@ router.post("/hora", (req,res) =>{
         res.json({message : error})
     })
 })
-
+//Obtener todos
 router.get("/hora", (req,res) =>{
     horarioSchema
     .find()
+    .then((data)=>{
+        res.json(data)
+    })
+    .catch((error)=>{
+        res.json({message : error})
+    })
+})
+//Obtener uno solo
+router.get("/hora/:id", (req,res) =>{
+    const {id} = req.params;
+    horarioSchema
+    .findById(id)
+    .then((data)=>{
+        res.json(data)
+    })
+    .catch((error)=>{
+        res.json({message : error})
+    })
+})
+//Actualizar
+router.put("/hora/:id", (req,res) =>{
+    const {id} = req.params;
+    const {hora_inicio_matutino, 
+        hora_cierre_matutino,
+        hora_inicio_vespertino,
+        hora_cierre_vespertino} = req.body;
+    horarioSchema
+    .updateOne({_id:id},{
+        $set:{
+            hora_inicio_matutino, 
+            hora_cierre_matutino,
+            hora_inicio_vespertino,
+            hora_cierre_vespertino
+        }
+    })
+    .then((data)=>{
+        res.json(data)
+    })
+    .catch((error)=>{
+        res.json({message : error})
+    })
+})
+//Eliminar
+router.delete("/hora/:id", (req,res) =>{
+    const {id} = req.params;
+    horarioSchema
+    .deleteOne({_id:id})
     .then((data)=>{
         res.json(data)
     })
