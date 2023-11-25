a188a308b5df2b28f647ef59d99cc2c9804cd026
MetodologiaDeSistemas <met.sistemas@gmail.com>
2023-11-25 13:18:43 -0300

    fecha dep crud

---
+++ src/Models/Fecha_Depilacion.js
@@ -0,0 +1,15 @@
+
+const { Int32 } = require("bson");
+const mongoose = require("mongoose");
+const fecha_dep = mongoose.Schema({
+    mes:{
+        type: String,
+        required: true
+    },
+    dia:{
+        type: Number,
+        required: true
+    },
+    
+});
+module.exports=mongoose.model('fecha_dep', fecha_dep)
